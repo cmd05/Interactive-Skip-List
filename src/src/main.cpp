be0149fde32a40d5b8e1592bc42e8339ce5aaa7c
cmd05 <splendid.snippet670@slmail.me>
2022-01-29 13:47:06 +0530

    Update main.cpp
    Console constructor

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -5,10 +5,10 @@
 
 int main()
 {
-    std::vector<double> vc = { 90, 15, -59, 48, -1, -28 };
-    Skip_List skip_list{};
+    std::vector<double> vc = {90, 15, -59, 48, -1, -28};
+    Skip_List skip_list{vc};
 
-    Console console{ skip_list };
+    Console console{skip_list, "output.txt"};
     console.write_to_file("output.txt");
     console.run();
 }
